3aa2c23a2df353c6107f8a6817029eba96de51ec
kavyashree-g <kavyashree.g@outlook.com>
2020-04-07 22:25:09 +1000

    Added more Controllers

---
+++ src/main/java/com/example/demo/controller/StoreApiController.java
@@ -0,0 +1,6 @@
+package com.example.demo.controller;
+
+import com.example.demo.api.spec.handler.StoreApi;
+
+public class StoreApiController implements StoreApi {
+}
---
+++ src/main/java/com/example/demo/controller/UserApiController.java
@@ -0,0 +1,6 @@
+package com.example.demo.controller;
+
+import com.example.demo.api.spec.handler.UserApi;
+
+public class UserApiController implements UserApi {
+}
