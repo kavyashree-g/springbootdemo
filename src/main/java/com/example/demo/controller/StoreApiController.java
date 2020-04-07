71dd054df7ff9a0f78b74116ea717fa6e1b18808
kavyashree-g <kavyashree.g@outlook.com>
2020-04-07 23:01:35 +1000

    Added methods to controllers

--- src/main/java/com/example/demo/controller/StoreApiController.java
+++ src/main/java/com/example/demo/controller/StoreApiController.java
@@ -1,6 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.api.spec.handler.StoreApi;
+import com.example.demo.api.spec.model.Order;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public class StoreApiController implements StoreApi {
+
+    public ResponseEntity<Order> getOrderById(@ApiParam(value = "ID of pet that needs to be fetched",required=true) @PathVariable("orderId") String orderId){
+        return null;
+    }
 }
--- src/main/java/com/example/demo/controller/UserApiController.java
+++ src/main/java/com/example/demo/controller/UserApiController.java
@@ -1,6 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.api.spec.handler.UserApi;
+import com.example.demo.api.spec.model.User;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public class UserApiController implements UserApi {
+
+    public ResponseEntity<User> getUserByName(@ApiParam(value = "The name that needs to be fetched. Use user1 for testing. ",required=true) @PathVariable("username") String username){
+        return null;
+    }
 }
